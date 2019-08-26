1a2a069be3276adf12ca4664c3c23801e92cb693
Kane Pangalia Stephens <kanepstephens@gmail.com>
2019-08-26 10:59:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # Your Code Here
+
+def map(array)
+  new_array = []
+  counter = 0
+  while counter < array.length do
+   new_array[counter] = yield(array[counter])
+    counter += 1
+  end
+  new_array
+end 
+
